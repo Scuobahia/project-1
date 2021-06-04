148973243b2387a687581ffbced598f80342ce36
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-06-03 21:23:52 -0600

    Added some psudo code in JS

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+
+function travelCLicked() {
+    // Handles user clicks on travelEstimates, this manages the request and sets the values for the buttons
+
+}
+
+function shipingClicked() {
+    // Handles when user clicks on Shipping estimates
+
+}
+
+function globalClicked() {
+    // Handles when user clicks on the shipping estimates
+
+}
+
+function travelElectricCar() {
+    // When Electric car is selected get 
+}
+
+function fetchTravel() {
+    //fetches from the API the 
+}
+

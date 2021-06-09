0dd8c1ee4b52e2bafa53b8668a7af321fef51232
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-06-08 21:54:17 -0600

    Completed Travel Estimates for vehicles. Give it a try! other than error handeling

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ function navCLicked(event) {
     // Handles user clicks on a nav bar button
   event.preventDefault();
   var targetId = event.target.getAttribute("id");
-  console.log(targetId)
   switch (targetId){
     case "title":
       displayNoneAll();
@@ -51,7 +50,7 @@ function displayNoneAll() {
   $("#shipping-page").addClass("d-none");
   $("#global-carbon-emissions-page").addClass("d-none");
   $("#climate-change-page").addClass("d-none");
-  $("#about-us-page").removeClass("d-none");
+  $("#about-us-page").addClass("d-none");
 }
 
 function footerClassChange() {
@@ -62,9 +61,12 @@ function travelElectricCar() {
     // When Electric car is selected get 
 }
 
-function getVehicleMake() {
+function getVehicleMake(event) {
   // gets user data from car form and finds the make, then if it can find it calls the getVehicleModel function.
-  var userMake = "tesla"; //$("#make").val();
+  event.preventDefault();
+  $("#results").html("<p class='px-2 mt-2 text-center' fw-bold>Loading your Results <div class='loader pb-2 my-auto mx-auto'</div></p>")
+  var userMake = $("#make").val();
+  console.log(userMake)
   userMake = userMake.trim().toLowerCase().split(" ");
     for (var i = 0; i < userMake.length; i++) {
         userMake[i] = userMake[i].charAt(0).toUpperCase() + userMake[i].substring(1);
@@ -101,14 +103,14 @@ function getVehicleMake() {
 
 function getVehicleModel(make, makeID) {
 //Gets the remaining info from the car form to find the model id to get the CO2 emeition information.
-  var userModel = "Model s"   //$("#model").val();
+  var userModel = $("#model").val();
   userModel = userModel.trim().toLowerCase().split(" ");
     for (var i = 0; i < userModel.length; i++) {
         userModel[i] = userModel[i].charAt(0).toUpperCase() + userModel[i].substring(1);
     }
     userModel = userModel.join(" ");
 
-    var userYear = "2012"//$("#year").val();
+    var userYear = $("#year").val();
     userYear = userYear.trim();
     userYear = parseInt(userYear);
 
@@ -132,7 +134,7 @@ function getVehicleModel(make, makeID) {
           alert("Error Vehicle model or year not found! Please Try again")
         }
         else {
-          vehicleEstimateRequest(car);
+          vehicleEstimateRequest(car, make, userModel, userYear);
         }
     }, error: function(data) {
       console.log(data);
@@ -140,10 +142,10 @@ function getVehicleModel(make, makeID) {
   })
 }
 
-function vehicleEstimateRequest(modelId) {
+function vehicleEstimateRequest(modelId, make, model, year) {
   //Posts the request to the API and gets the carbon emitions
-  var distanceUnit = "mi" //$("#distance-unit").val();
-  var distanceValue = "100" //$("#distance-value").val();
+  var distanceUnit = $("#distance-unit").val();
+  var distanceValue = $("#distance-value").val();
   distanceValue = parseInt(distanceValue.trim());
 
   fetch("https://www.carboninterface.com/api/v1/estimates", {
@@ -161,6 +163,15 @@ function vehicleEstimateRequest(modelId) {
   }).then((response) => response.json())
   .then((data) => {
     console.log(data);
+    if (distanceUnit === "mi") {
+      var unit = "Miles";
+    }
+    else if(distanceUnit === "km") {
+      var unit = "Kilometers";
+    }
+    var carbonEmitions = data.data.attributes.carbon_lb;
+    $("#results")
+    .html("<p class='py-2 px-2 text-center'>Your total cabon emitions for your <span class='main-color fw-bold'>" + make + " " + model + " " + year + "</span> after driving <span class='main-color fw-bold'>" + distanceValue + " " + unit + "</span> would be: <span class='main-color fw-bold'>" + carbonEmitions + " Pounds</span> of CO2 released in to the atmosphere. </p>" );
   });
 }
 
@@ -230,6 +241,18 @@ function shippingEstimateRequest() {
 }
 
   $("#header").on("click", navCLicked);
+  //Travel Estimates events
+  $("#vehicle-btn").on("click", function(event) {
+    event.preventDefault();
+    $("#vehicle-form").removeClass("d-none");
+    $("#flight-form").addClass("d-none");
+  });
+  $("#flight-btn").on("click", function(event) {
+    event.preventDefault();
+    $("#vehicle-form").addClass("d-none");
+    $("#flight-form").removeClass("d-none");
+  });
+  $("#vehicle-form").on("submit", getVehicleMake);
 
   
 
